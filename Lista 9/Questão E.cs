325c726965ebfaf597e61fbf25c3cb142fc3d49d
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-02-10 10:26:22 -0300

    Create Questão E.cs

---
+++ Lista 9/Questão E.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+	class MinhaClasse
+{
+   public static void Main() 
+    {
+        
+   	
+   		int[] vetor = {10, 20, 30, 40, 50};
+   		
+   		int menor = vetor.Min();
+			
+			Console.WriteLine("O menor dos números é: " +menor);
+			Console.ReadKey();
+			
+    }
+}

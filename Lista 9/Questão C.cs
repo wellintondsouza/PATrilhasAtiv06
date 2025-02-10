e381edc1c488b06a01346ef910cf0e5ea7db1985
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-02-10 10:24:13 -0300

    Create Questão C.cs

---
+++ Lista 9/Questão C.cs
@@ -0,0 +1,17 @@
+	using System;
+
+class MinhaClasse
+{
+   public static void Main() 
+    {
+        
+   	
+   		int[] vetor = {10, 20, 30, 40, 50};
+   		
+   		int maior = vetor.Max();
+			
+			Console.WriteLine("O maior dos números é: " +maior);
+			Console.ReadKey();
+			
+    }
+}
